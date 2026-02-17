e090658e2f62d2fec4ae75610d8064bf7ac987a0
Artamonov <sirseul@gmail.com>
2026-02-17 14:59:37 +0400

    Добавлен скрипт для получения случайного изображения

---
+++ fetch_painting.py
@@ -0,0 +1,49 @@
+import csv
+import random
+import urllib.request
+import json
+import os
+
+
+def get_painting(csv_path):
+    paintings = []
+    with open(csv_path, mode='r', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row.get('Classification') == 'Paintings' and row.get('Is Public Domain') == 'True':
+                paintings.append(row)
+    return random.choice(paintings)
+
+def fetch_object_metadata(object_id):
+    url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}"
+    with urllib.request.urlopen(url) as response:
+        data = json.loads(response.read().decode('utf-8'))
+    return data
+
+def download_image(image_url, save_path):
+    urllib.request.urlretrieve(image_url, save_path)
+
+def save_metadata(data, save_path):
+    with open(save_path, 'w', encoding='utf-8') as path:
+        json.dump(data, path, indent=2, ensure_ascii=False)
+
+def main():
+    csv_path = 'MetObjects.csv'
+    output_dir = 'paintings'
+    os.makedirs(output_dir, exist_ok=True)
+    painting = get_painting(csv_path)
+
+    object_id = painting.get("Object ID")
+
+    metadata = fetch_object_metadata(object_id)
+    primary_image = metadata.get('primaryImage')
+
+    filename = str(object_id)
+    img_path = os.path.join(output_dir, filename + '.jpg')
+    json_path = os.path.join(output_dir, filename + '.json')
+
+    download_image(primary_image, img_path)
+    save_metadata(metadata, json_path)
+
+if __name__ == '__main__':
+    main()
